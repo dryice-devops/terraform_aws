5a7df4fe987be81cc2e8313feeae54f497983e27
abhijeet150793 <82080978+abhijeet150793@users.noreply.github.com>
2021-04-07 17:00:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
+
 resource "aws_instance" "instance" {
-  ami                    = "${var.image_id}"
-  instance_type          = "${var.Instancetype}"
+  ami                    = data.aws_ami.centos.id
+  instance_type          = var.Instancetype
   associate_public_ip_address = "true"
   monitoring             = "true"
-  subnet_id  = "${var.subnet_id}"
-  security_groups = ["sg-0fa96daa32a366653"]
+  key_name = var.key_name
+  subnet_id  = var.subnet_id
+  security_groups = ["${var.vpc_security_group_ids}"]
 tags = {
-    Name = "${var.name}"
-    Environment = "${var.environment}"
-    Business_Justification = "${var.bJustification}"
-    Reason = "${var.reason}"
+    Name = var.name
+    Environment = var.environment
+    Business_Justification = var.bJustification
+    Reason = var.reason
    }
  }
